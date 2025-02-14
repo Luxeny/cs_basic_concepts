cc5026c6230a6defa4a66266a2b0ad7ae540edc2
Виталий <148930900+Luxeny@users.noreply.github.com>
2025-02-15 03:35:05 +0700

    Solution added.

---
+++ cs_basic_concepts.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine("Выберите задание:");
+        WriteLine("1. Вычислить a^n");
+        WriteLine("2. Найти число n после манипуляций с числом x");
+        Write("Введите номер задания (1 или 2): ");
+        int choice = int.Parse(ReadLine());
+
+        if (choice == 1)
+        {
+            // Задание 1: Вычисление a в степени n
+            Write("Введите основание степени a: ");
+            int baseNumber = int.Parse(ReadLine());
+
+            Write("Введите показатель степени n: ");
+            int exponent = int.Parse(ReadLine());
+
+            int powerResult = Power(baseNumber, exponent);
+            WriteLine($"{baseNumber}^{exponent} = {powerResult}");
+        }
+        else if (choice == 2)
+        {
+            // Задание 2: Нахождение числа n после манипуляций с числом x
+            Write("Введите число x (x >= 100): ");
+            string inputNumber = ReadLine();
+
+            if (inputNumber.Length < 3)
+            {
+                WriteLine("Число x должно быть не менее 3 цифр.");
+                return;
+            }
+
+            char secondDigit = inputNumber[1];
+            string numberWithoutSecondDigit = inputNumber.Remove(1, 1);
+            string resultNumber = numberWithoutSecondDigit + secondDigit;
+
+            WriteLine($"Полученное число: {resultNumber}");
+        }
+        else
+        {
+            WriteLine("Неверный выбор. Пожалуйста, выберите 1 или 2.");
+        }
+    }
+
+    // Метод для вычисления степени числа
+    static int Power(int baseNumber, int exponent)
+    {
+        int result = 1;
+        for (int iterationNumber = 0; iterationNumber < exponent; ++iterationNumber)
+        {
+            result *= baseNumber;
+        }
+        return result;
+    }
+}
